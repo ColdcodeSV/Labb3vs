7fa2b07da9f9f4d49c7accc893d9312547a35282
Crazybane <112276387+ColdcodeSV@users.noreply.github.com>
2024-03-28 01:28:29 +0100

    Implemented game restart and reset functionality

--- game-end-functions.ino
+++ game-end-functions.ino
@@ -18,3 +18,39 @@ delay(3000);
 // Återstarta spelet
 restartGame();
 }
+void restartGame() {
+// Återställ alla spelvariabler till startvärden
+money = 100;
+wins = 0;
+losses = 0;
+bet = 10;
+headsOrTail = "";
+choice = '\0';
+gameOver = false;
+nameEntered = false;
+playerName = "";
+
+// Rensa LCD-skärmen och börja om processen för att få spelarens namn
+lcd.clear();
+getPlayerName();
+}
+
+void resetGame() {
+// Visa ett meddelande om att spelet återställs och att det kan spelas igen, vänta 2 sekunder
+lcd.setCursor(0, 0);
+lcd.print("GAME RESETS");
+lcd.setCursor(0, 1);
+lcd.print("PLAY AGAIN!");
+delay(2000);
+
+// Återställ alla spelvariabler till startvärden, rensa LCD-skärmen och visa spelstatus
+money = 100;
+wins = 0;
+losses = 0;
+bet = 10;
+headsOrTail = "";
+choice = '\0';
+gameOver = false;
+lcd.clear();
+displayStatus();
+}
